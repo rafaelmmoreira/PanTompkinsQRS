f550b1794a87c693ae102b02732728e9f22128d7
ttronrud <ttronrud@starfishmedical.com>
2022-09-20 13:40:20 -0700

    moved test data and code to external main

--- PanTompkinsEmbedded.h
+++ PanTompkinsEmbedded.h
@@ -12,6 +12,30 @@
 #include <stdint.h>
 typedef int dataType;
 
+
+//A struct definition for the filter state
+//to allow storing/loading of filters in case
+//a period of bad data ruins performance
+typedef struct {
+	int rr1[8];
+	int rr2[8];
+	int rravg1;
+	int rravg2;
+	int rrlow;
+	int rrhigh;
+	int rrmiss;
+	dataType peak_i;
+	dataType peak_f;
+	dataType threshold_i1;
+	dataType threshold_i2;
+	dataType threshold_f1;
+	dataType threshold_f2;
+	dataType spk_i;
+	dataType spk_f;
+	dataType npk_i;
+	dataType npk_f;
+} FilterState;
+
 /*
  * Execute PT on the initialized signal
  * If a signal has been executed, it will re-run with the
@@ -29,7 +53,7 @@ void PanTompkins();
  * signal (from a new source). Shouldn't be necessary more than once,
  * but wiping all learning from a poor signal may be useful, who knows.
  */
-void PanTompkins_init(dataType *signal, uint32_t sig_len);
+void PanTompkins_init(dataType *signal, uint32_t sig_len, int *Rs_out, int in_RS_LEN);
 /*
  * Swap the active signal source - maintaining buffers and RR averages
  * but directing the flow at a new signal. Not necessary if you are using the
@@ -40,13 +64,18 @@ void PanTompkins_init(dataType *signal, uint32_t sig_len);
  * set to zero. If you are planning to do an RRI calculation from pieces of ECG,
  * use -1 (or the end of the last R array).
  */
-void PanTompkins_sigswap(dataType *new_signal, uint32_t new_sig_len, uint32_t new_index_offset, uint32_t r_index_start);
+void PanTompkins_sigswap(dataType *new_signal, uint32_t new_sig_len, uint32_t new_index_offset, uint32_t r_index_start, int *new_Rs, int new_RS_LEN);
 
 /*
  * Saving and loading of the filterstate variable.
  * Could be useful in the case of absolutely horrendous data
  * that ruins the state of the thresholds.
+ * Also can set the internally-stored filterstates with
+ * an external source, or export the internal FS to a
+ * different source
  */
+void PanTompkins_setsaved_filterstate(FilterState *fs);
+void PanTompkins_exportsaved_filterstate(FilterState *fs);
 void PanTompkins_save_filterstate();
 void PanTompkins_load_filterstate();
 
